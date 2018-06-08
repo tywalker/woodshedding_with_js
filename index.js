be2c39eca382eaed4e20f58c52f194ce20d0a194
Tyler Walker <tyler.walker@dakno.com>
2018-06-08 00:22:33 -0400

    Bubble sort done for OO moving on to functional.

--- index.js
+++ index.js
@@ -2,33 +2,31 @@
 // Bubble Sort
 ///////
 
-let arr = [4,5,3,8,9,1,3,2]
+let myArr = [4,5,3,8,9,1,7,25,54,62,67,34,12,98,104]
 
-let sortedArr = arr.reduce(
-  (acc, cur, i, arr) => {
-      if (i === 0) { acc = arr}
-      else { return acc }
-  }
-  ,[]
-);
-
-// console.log(sortedArr)
-// for (var i = 1; i <= arr.length; i++) {
-//   if (arr[i] < arr[i - 1]) {
-//     let num = arr.slice(i - 1);
-//     arr.splice(i - 1, 0, arr[i - 1]);
-//     arr.splice(i, 1, num);
+// let sortedArr = arr.reduce(
+//   (acc, cur, i, arr) => {
+//       if (i === 0) { acc = arr}
+//       else { return acc }
 //   }
-// }
+//   ,[]
+// );
 
-// XOR algorithm
-let a = arr[0]
-let b = arr[1]
+/**
+ * OO style recursion
+ * @param  {[type]} arr [description]
+ * @return {[type]}     [description]
+ */
+const swapLeft = (arr) => {
+  let swaps = false;
+  for (var i = 1; i <= arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      swaps = true
+      arr[i - 1] ^= arr[i], arr[i] ^= arr[i - 1], arr[i - 1] ^= arr[i]
+    }
+  }
 
-console.log(a, b)
+  swaps ? swapLeft(arr) : console.log(arr)
+}
 
-a ^= b
-b ^= a
-a ^= b
-
-console.log(a, b)
+swapLeft(myArr)
