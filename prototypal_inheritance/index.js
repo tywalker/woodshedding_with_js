2fd29e276ccf56691b0d0499f602f0b1aecedc99
Tyler Walker <tyler.walker@dakno.com>
2018-06-14 23:42:14 -0400

    Added example for prototypal inheritance.

---
+++ prototypal_inheritance/index.js
@@ -0,0 +1,20 @@
+// Object inheritance with concatenation
+//
+
+const furry = { furry: true }
+const meows = { meows: "meow meow" }
+const barks = { barks: "bork bork" }
+const slinks = { slinks: true }
+const isAClutz = { isAClutz: true }
+
+const Dog = (opts) => {
+  return Object.assign({}, furry, barks, isAClutz)
+}
+
+const Cat = (opts) => {
+  return Object.assign({}, furry, meows, slinks)
+}
+
+const casey = Cat({ furry, meows, slinks})
+console.log(casey.furry)
+console.log(casey.meows)
--- this_practice/index.js
+++ this_practice/index.js
@@ -2,4 +2,29 @@ console.log(this)
 
 const f = (x, y) => x + y;
 
-console.log(f(4,3))
+var name = {
+  x: "bil",
+  y: "billy",
+  xY: function() {
+    return this.x
+  },
+  yX: function() {
+    return this.y
+  }
+}
+
+function returnThing( x, callback ) {
+  console.log(this)
+  callback(x)
+}
+
+function sayThing(x) {
+  console.log(this)
+  console.log(x)
+}
+
+returnThing("Bio", sayThing)
+
+console.log(name.x)
+console.log(name.y)
+console.log(name.xY())
