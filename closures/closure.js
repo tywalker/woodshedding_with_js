af0cc04e43fe262fbdc46da747ac4c7bf95488c0
Tyler Walker <tyler.walker@dakno.com>
2018-07-11 23:31:56 -0400

    Added curried function. Understanding closures as well

--- closures/closure.js
+++ closures/closure.js
@@ -1,1 +1,22 @@
 console.log("working")
+
+const closure = (x, y) => {
+  var h = "hoisted";
+  x = 24;
+  y = 25;
+  return (z) => {
+    let add;
+    if (z) {
+      add = x + y + z;
+    } else {
+      add = x + y;
+    }
+    return add
+  }
+
+  return x + y;
+}
+
+let c = closure(24, 68);
+
+console.log(c)
